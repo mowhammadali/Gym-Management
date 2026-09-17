f6af2ed2697272f9f4a827f28f270f72fc8a8d02
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-17 11:17:08 +0330

    implement contracts and subscriptions controller

---
+++ GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -0,0 +1,15 @@
+﻿using GymManagement.Contracts.Subscriptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagement.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SubscriptionsController : Controller
+{
+    [HttpPost]
+    public IActionResult CreateSubscription([FromBody] CreateSubscriptionRequest request)
+    {
+        return Ok(request);
+    }
+}
