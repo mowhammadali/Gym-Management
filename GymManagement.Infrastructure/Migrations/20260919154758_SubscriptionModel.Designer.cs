ab2397c289bcd01db0f9f6b244dd30347b10bcae
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-19 20:55:03 +0330

    add subscription model migration

--- GymManagement.Infrastructure/Migrations/20260919154758_SubscriptionModel.Designer.cs
+++ GymManagement.Infrastructure/Migrations/20260919154758_SubscriptionModel.Designer.cs
@@ -4,6 +4,7 @@ using GymManagement.Infrastructure.Common.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GymManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(GymManagementDbContext))]
-    partial class GymManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260919154758_SubscriptionModel")]
+    partial class SubscriptionModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
